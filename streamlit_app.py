d3fbb74d2ef48bc80f03ea1d4b9e506437d6716c
dhritirathod <115212825+dhritirathod@users.noreply.github.com>
2022-10-10 13:18:24 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from prediction_module import predict_class
+
+st.set_page_config(page_title="SQLi Detection",page_icon="👾",layout="wide")	# Set the page title and icon
+
+st.title("SQLi Detection")																																			# Set the title of the page
+st.text("""Please enter the SQL query in the text box below to detect if this query is a SQLi attack.""")	# Set the text of the page
+
+st.header("Enter SQL query")												# Set the header of the page
+with st.form("SQLi Detection"):											# Create a form with name "SQLi Detection"
+		query = st.text_input("Enter SQL query here")		# Create a text input with name "Enter SQL query here"
+		if st.form_submit_button("Predict"):							# if the user clicks the submit button
+			isSQLi = predict_class(query)									# Call the predict_class function and store the result in isSQLi
+			st.write("Your query is:", query)							# Write the query in the text box
+			if isSQLi:																		# If the query is a SQLi attack
+				st.write("This is a SQLi attack")						# Write "This is a SQLi attack"
+			else:																					
+				st.write("This is not a SQLi attack")				# else write "This is not a SQLi attack"
